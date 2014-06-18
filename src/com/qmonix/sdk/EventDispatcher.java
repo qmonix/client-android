349685b45d306d5f7086b0d3fe343b039e4a5d0c
Povilas Balciunas <balciunas90@gmail.com>
2014-06-18 19:06:10 +0300

    Adds dispatch() method to EventDispatcher interface.

--- src/com/qmonix/sdk/EventDispatcher.java
+++ src/com/qmonix/sdk/EventDispatcher.java
@@ -1,12 +1,13 @@
 package com.qmonix.sdk;
 
+
 /**
- * Event dispatcher interface which is responsible for dispatching events to the server. Exact
- * behaviour depends on specific implementation, which can store event locally, send them to the
- * server, etc..
+ * Event dispatcher is responsible for collecting and dispatching events. Sample dispatchers:
+ * file dispatcher that stores events locally in file system; HTTP dispatcher which sends events to
+ * Qmonix Web service, log dispatcher that sends collected events to log output, etc.
  * <p>
- * Dispatcher interface has a single method {@link #submit submit} which accepts event object for
- * further processing.
+ * Use {@link #submit submit} to pass events to event dispatcher. Use {@link #dispatch dispatch}
+ * to flush the collected events meaning to act appropriately to specific dispatcher logic.
  *
  * @see Event
  */
@@ -18,4 +19,11 @@ public interface EventDispatcher
 	 * @param event event object to dispatch.
 	 */
 	public void submit(Event event);
+
+	/**
+	 * Send, store, print collected events.
+	 *
+	 * @param handler successful or failed dispatch handler. Might be null.
+	 */
+	public void dispatch(EventDispatchHandler handler);
 }
